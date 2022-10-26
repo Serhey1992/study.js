d5cc284f8c1605b79867548c935e753ce819bf70
Serhey1992 <makss92i@gmail.com>
2022-10-26 22:58:07 +0300

    ++

---
+++ js/first-lesson.js
@@ -0,0 +1,108 @@
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const dif = apples - grapes;
+// console.log(dif);
+
+// let studets = 100;
+// studets += 50;
+// console.log(studets);
+
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
+// const addition = 108 + 223;
+// console.log(addition);
+// const multiplication = 2 * 5;
+// console.log(multiplication);
+// const result = addition - multiplication;
+// console.log(result);
+
+// const value = 27.5;
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value));
+
+// const companyName = "Cyberdyne Syst";
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock.`
+// console.log(message);
+
+// let weight = "88.3";
+// let height = "1.75";
+// weight = Number.parseFloat(weight);
+// height = Number.parseFloat(height);
+// heightPow = Math.pow(1.75, 2);
+// console.log(heightPow);
+// const bmi = weight / heightPow;
+// console.log(bmi.toFixed(1));
+
+
+// console.log(5 > 4);
+
+// console.log(10 >= '7');
+
+// console.log('2' > '12');
+
+// console.log('2' < '12');
+
+// console.log('4' == 4);
+
+// console.log('6' === 6);
+
+// console.log('false' === false);
+
+// console.log(1 == true);
+
+// console.log(1 === true);
+
+// console.log('0' == false);
+
+// console.log('0' === false);
+
+// console.log('Papaya' < 'papaya');
+
+// console.log('Papaya' === 'papaya');
+
+// console.log(undefined == null);
+
+// console.log(undefined === null);
+
+
+
+// console.log(true && 3);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && 'kiwi');
+
+// console.log(true && 0 && 'kiwi');
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
+
+// const incomingValue = 5;
+// const defaultValue = 10;
+// const value = incomingValue || defaultValue;
+// console.log(value);
+
+
+
+// const totalMinutes = 70;
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
---
+++ js/second-lesson.js
@@ -0,0 +1,27 @@
+// let javaName = prompt("Яка офіціна назва JavaScript?")
+// console.log(javaName);
+// if (javaName ==="ECMASScript") {
+//     alert("Вірно!");
+// } else {
+//     alert("Не знаєте? ECMASScript!")
+// }
+
+// const hours = 14;
+// const minutes = 0;
+// let timestring;
+
+// if (minutes > 0) {
+//     message = `${hours} ч. ${minutes} мин.`
+// } else {
+//     message = `${hours}ч.`
+// }
+// console.log(message);
+
+// let inputNumber = prompt("Input your number!");
+// if (inputNumber > 0) {
+//     alert("Good number")
+// } else if (inputNumber === 0) {
+//     alert("Its a zero")
+// } else {
+//     alert("not good number!")
+// }
